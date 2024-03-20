d85d3c4e32adf85070c60d3b149f88e0c7e24e0c
BastiaanMeijer <bastiaanm@digifit.eu>
2024-03-20 19:33:44 +0100

    fixed several Quality Gate issues.

--- Hive/src/Game.php
+++ Hive/src/Game.php
@@ -49,6 +49,9 @@ class Game
             case "Pass":
                 echo "pass";
                 break;
+            default:
+                $this->restartGame();
+
         }
 
         GameView::render();
