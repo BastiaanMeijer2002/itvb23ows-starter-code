36aaabab1901af824c4f003f1d460d2a17f6b5a8
BastiaanMeijer <bastiaanm@digifit.eu>
2024-03-21 11:16:03 +0100

    fixed and added unit tests

--- Hive/tests/GameUtilsTest.php
+++ Hive/tests/GameUtilsTest.php
@@ -1,6 +1,7 @@
 <?php
 
 
+use HiveGame\GameState;
 use HiveGame\GameUtils;
 use PHPUnit\Framework\TestCase;
 
@@ -10,7 +11,7 @@ class GameUtilsTest extends TestCase
         $board = [];
         $expectedPossiblePlays = ["0,0"];
 
-        $actualPossiblePlays = GameView::getPossiblePlays($board);
+        $actualPossiblePlays = GameUtils::getPossiblePlays($board);
 
         $this->assertEquals($expectedPossiblePlays, $actualPossiblePlays);
     }
@@ -19,7 +20,7 @@ class GameUtilsTest extends TestCase
         $board = ["0,0" => []];
         $expectedPossiblePlays = ['0,1', '1,0', '-1,0', '0,-1', '1,-1', '-1,1'];
 
-        $actualPossiblePlays = GameView::getPossiblePlays($board);
+        $actualPossiblePlays = GameUtils::getPossiblePlays($board);
 
         $this->assertEquals($expectedPossiblePlays, $actualPossiblePlays);
     }
@@ -28,8 +29,19 @@ class GameUtilsTest extends TestCase
         $board = ["0,0" => [], "0,1" => []];
         $expectedPossiblePlays = ["0,-1", "1,0", "-1,0", "-1,1", "1,-1", "0,2", "1,1", "-1,2"];
 
-        $actualPossiblePlays = GameView::getPossiblePlays($board);
+        $actualPossiblePlays = GameUtils::getPossiblePlays($board);
 
         $this->assertEqualsCanonicalizing($expectedPossiblePlays, $actualPossiblePlays);
+    }
+
+    public function testPlayerTiles() {
+        $board = ["0,0" => [[0, "Q"]], "0,1" => [[1, "Q"]],"0,2" => [[0, "S"]]];
+        $player = 0;
+        GameState::setPlayer($player);
+        $expectedTiles = ["0,0", "0,2"];
+
+        $actualTiles = GameUtils::getPlayerTiles($board);
+
+        $this->assertEquals($expectedTiles, $actualTiles);
     }
 }
