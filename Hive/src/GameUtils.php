f3b5b912b6cc560914ac6a2cdb532b1feb8cca65
BastiaanMeijer <bastiaanm@digifit.eu>
2024-03-28 20:58:50 +0100

    attempt to fix Quality Gate issues

--- Hive/src/GameUtils.php
+++ Hive/src/GameUtils.php
@@ -6,69 +6,54 @@ class GameUtils
 {
     public static function checkWin($board): ?int
     {
-        $queenBeePlayer1 = null;
-        $queenBeePlayer2 = null;
+        $queenBeePlayer1 = self::findQueenBee($board, 0);
+        $queenBeePlayer2 = self::findQueenBee($board, 1);
 
-        foreach ($board as $tile => $item){
-            if ($item[0][1] == "Q") {
-                if ($item[0][0] == 0) {
-                    $queenBeePlayer1 = $tile;
-                } else {
-                    $queenBeePlayer2 = $tile;
-                }
-            }
-        }
+        $countPlayer1 = $queenBeePlayer2 ? self::countSurroundingTiles($board, $queenBeePlayer2, 0) : 0;
+        $countPlayer2 = $queenBeePlayer1 ? self::countSurroundingTiles($board, $queenBeePlayer1, 1) : 0;
 
-        $countPlayer1 = 0;
-        $countPlayer2 = 0;
-
-        if ($queenBeePlayer2 != null) {
-            $tiles = self::getSurroundingTiles($queenBeePlayer2);
-            foreach ($tiles as $tile) {
-                if ($board[$tile][0][0] == 0) {
-                    $countPlayer1++;
-                }
-            }
-        }
-
-        if ($queenBeePlayer1 != null) {
-            $tiles = self::getSurroundingTiles($queenBeePlayer1);
-            foreach ($tiles as $tile) {
-                if ($board[$tile][0][0] == 1) {
-                    $countPlayer2++;
-                }
-            }
-        }
-
-        $result = null;
         if ($countPlayer1 == 6 && $countPlayer2 < 6) {
-            $result = 0;
+            return 0;
         } elseif ($countPlayer2 == 6 && $countPlayer1 < 6) {
-            $result = 1;
+            return 1;
         } elseif ($countPlayer1 == 6 && $countPlayer2 == 6) {
-            $result = 3;
+            return 3;
         }
 
-        return $result;
-
+        return null;
     }
 
-    public static function getSurroundingTiles($target): array
+    public static function findQueenBee($board, $player): ?string
     {
-        $targetCoords = explode(",", $target);
-        $x = $targetCoords[0];
-        $y = $targetCoords[1];
-
-        $offsets = [[0, 1], [0, -1], [1, 0], [-1, 0], [-1, 1], [1, -1]];
-        $surroundingCoords = [];
-
-        foreach ($offsets as $offset) {
-            $surroundingX = $x + $offset[0];
-            $surroundingY = $y + $offset[1];
-            $surroundingCoords[] = $surroundingX .",". $surroundingY;
+        foreach ($board as $tile => $item) {
+            if ($item[0][1] == "Q" && $item[0][0] == $player) {
+                return $tile;
+            }
         }
+        return null;
+    }
 
-        return $surroundingCoords;
+    public static function countSurroundingTiles($board, $queenBeeTile, $player): int
+    {
+        $count = 0;
+        $tiles = self::getSurroundingTiles($queenBeeTile);
+        foreach ($tiles as $tile) {
+            if ($board[$tile][0][0] == $player) {
+                $count++;
+            }
+        }
+        return $count;
+    }
+
+    public static function getSurroundingTiles($tile): array
+    {
+        $offsets = [[0, 1], [0, -1], [1, 0], [-1, 0], [-1, 1], [1, -1]];
+        $surroundingTiles = [];
+        list($x, $y) = explode(",", $tile);
+        foreach ($offsets as $offset) {
+            $surroundingTiles[] = ($x + $offset[0]) . "," . ($y + $offset[1]);
+        }
+        return $surroundingTiles;
     }
 
 
